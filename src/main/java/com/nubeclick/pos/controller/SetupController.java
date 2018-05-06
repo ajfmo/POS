a6979bdf32264f3c158db49d56e98771a86b10d0
Adrian Flores <aj.fm@hotmail.com>
2018-05-06 11:17:57 -0400

    Code refactor.

--- src/main/java/com/nubeclick/pos/controller/SetupController.java
+++ src/main/java/com/nubeclick/pos/controller/SetupController.java
@@ -3,17 +3,17 @@ package com.nubeclick.pos.controller;
 import java.io.Serializable;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.nubeclick.pos.dto.Role;
 import com.nubeclick.pos.repository.RoleRepository;
 
-@Controller
+@Service
 public class SetupController<ID extends Serializable> {
 
 	@Autowired
-	private RoleRepository<Role> roleRepository;
+	private RoleRepository roleRepository;
 
 	private Role role = new Role();
 
--- src/main/java/com/nubeclick/pos/repository/RoleRepository.java
+++ src/main/java/com/nubeclick/pos/repository/RoleRepository.java
@@ -3,13 +3,9 @@ package com.nubeclick.pos.repository;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 import com.nubeclick.pos.dto.Role;
 
-@Repository
-public interface RoleRepository<R> extends CrudRepository<Role, Long> {
-
+public interface RoleRepository extends CrudRepository<Role, Long> {
 	List<Role> findByRoleDescription(String roleDescription);
-
 }
