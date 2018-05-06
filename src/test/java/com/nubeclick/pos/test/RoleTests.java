176dd8a1703031d3225855c4c6ceeb0542ed3b34
Adrian Flores <aj.fm@hotmail.com>
2018-05-06 10:54:35 -0400

    Added Test to Role entity.

--- src/test/java/com/nubeclick/pos/test/RoleTests.java
+++ src/test/java/com/nubeclick/pos/test/RoleTests.java
@@ -1,5 +1,36 @@
 package com.nubeclick.pos.test;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.nubeclick.pos.dto.Role;
+import com.nubeclick.pos.repository.RoleRepository;
+
+@RunWith(SpringRunner.class)
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@DataJpaTest
 public class RoleTests {
+
+	@Autowired
+	private RoleRepository roleRepo;
+
+	private Role role;
+
+	@Test
+	public void contextLoads() {
+	}
+
+	@Test
+	public void saveRole() {
+		role = new Role("Dios", 99);
+		roleRepo.save(role);
+		Assert.assertNotNull(roleRepo.findByRoleDescription("Dios"));
+	}
 
 }
